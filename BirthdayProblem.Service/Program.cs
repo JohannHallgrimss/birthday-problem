32a1b39cea51660bb0d3801d4e941e1a7fc95c00
forri <forri@DESKTOP-37855GD>
2025-09-11 01:24:01 +0000

    smá einföldun á input path meðhöldun

--- BirthdayProblem.Service/Program.cs
+++ BirthdayProblem.Service/Program.cs
@@ -6,14 +6,18 @@ class Program
     {
         if (string.IsNullOrWhiteSpace(args[0]))
             throw new Exception("Imput file not provided");
-        else if (!args[0].ToLower().EndsWith(".json") && !args[0].ToLower().EndsWith(".csv"))
-            throw new Exception("Imput filetype not supported");
+        string extention = Path.GetExtension(args[0]).ToLower(); 
+        
 
-        var today = DateTime.Today;// new DateTime(2025, 2, 28);
+        List<Person> persons = extention switch
+        {
+            ".json" => PersonJsonAdapter.GetPersonsFromJson(args[0]),
+            ".csv" => PersonCsvAdapter.GetPersonsFromCsv(args[0]),
+            _ => throw new Exception("Imput filetype not supported")
+        };
+
+        var today = DateTime.Today; // new DateTime(2025, 2, 28);
         var birthdayService = new BirthdayService(today);
-
-        List<Person> persons = args[0].ToLower().EndsWith(".json") ? PersonJsonAdapter.GetPersonsFromJson(args[0]) : PersonCsvAdapter.GetPersonsFromCsv(args[0]);
-
         persons.ForEach(person =>
         {
             if (birthdayService.IsBirthdayToday(person.DateOfBirth))
